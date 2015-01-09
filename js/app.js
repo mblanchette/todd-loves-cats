60bc5ea073b0edce9a6bb3cad62116368898bff0
Matt Blanchette <matthew-blanchette@idexx.com>
2015-01-09 00:09:26 -0500

    added mp3 audio files
    * added audio html5 for 4 different songs and different views
    * added logic in app js to turn down volume by default, using trigger on changing view

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
 var tlcApp = angular.module('tlcApp', []);
 
-tlcApp.controller('MainCtrl', function ($scope) {
+tlcApp.controller('MainCtrl', function ($scope,$timeout) {
   var currentDate = new Date();
   // Special rendering of ui for these params
   $scope.sitedown404 = false;
   $scope.retro = true;
   $scope.xmas = currentDate.getMonth() === 11 && currentDate.getDate() >= 25; // months are 0-11, starting at jan
   $scope.saturday = currentDate.getDay() === 6; // days are 0-6, starting at sunday
-   
+  
+  $scope.changedView = function() {
+    // turn down audio volume slightly by default
+    $timeout(function(){
+       $( "audio" ).each(function(index,element) {
+         if( element.id === 'audio4-xmas' ) {
+           this.volume = 0.2;
+         } else {
+           this.volume = 0.5;
+         }
+       });
+    });
+  };
+  // For initial page load
+  $scope.changedView();
+  
   var sections = [
     {'name': 'Time',
      'heading': 'Time spent with cats is never wasted',
